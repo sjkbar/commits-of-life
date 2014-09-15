74846a665d9161b984426266959f659e7adf1425
Jun Gyoung Seong <jungyoung.seong@gmail.com>
2014-09-15 21:16:36 +0900

    Add TestCase for hw1.

--- 2014-fall/ProgrammingLanguage/hw1/hw1_2.ml
+++ 2014-fall/ProgrammingLanguage/hw1/hw1_2.ml
@@ -69,6 +69,31 @@ print_endline (string_of_int (sum_of_tree (Tree (Tree (Leaf 3, Leaf 4), Tree (Tr
 ;;
 print_endline (string_of_int (sum_of_tree (Leaf 5)))
 ;;
+
+let _ = 
+let print_bool x = 
+print_endline (string_of_bool x) in 
+print_bool (true = eval TRUE); 
+print_bool (false = eval FALSE); 
+print_bool (false = eval (NOT TRUE)); 
+print_bool (true = eval (NOT FALSE)); 
+print_bool (true = eval (ANDALSO (TRUE, TRUE))); 
+print_bool (false = eval (ANDALSO (TRUE, FALSE))); 
+print_bool (false = eval (ANDALSO (FALSE, TRUE))); 
+print_bool (false = eval (ANDALSO (FALSE, FALSE))); 
+print_bool (true = eval (ORELSE (TRUE, TRUE))); 
+print_bool (true = eval (ORELSE (TRUE, FALSE))); 
+print_bool (true = eval (ORELSE (FALSE, TRUE))); 
+print_bool (false = eval (ORELSE (FALSE, FALSE))); 
+print_bool (false = eval (IMPLY (TRUE, FALSE))); 
+print_bool (true = eval (IMPLY (TRUE, TRUE))); 
+print_bool (true = eval (IMPLY (FALSE, TRUE))); 
+print_bool (true = eval (IMPLY (FALSE, FALSE))); 
+print_bool (true = eval (LESS (NUM 3, NUM 5))); 
+print_bool (false = eval (LESS (NUM 3, NUM 3))); 
+print_bool (false = eval (LESS (NUM 3, NUM 1))); 
+print_bool (false = eval 
+(LESS (PLUS (NUM 3, NUM 4), MINUS (NUM 5, NUM 1)))); 
+print_bool (true = eval 
+(LESS (PLUS (NUM 10, NUM 12), MINUS (NUM 10, NUM (-13))))); 
 *)
-
-

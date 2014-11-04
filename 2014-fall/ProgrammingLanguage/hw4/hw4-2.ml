2d999d1fc7205626913d830e2e044e91bd848973
Jun Gyoung Seong <jungyoung.seong@gmail.com>
2014-11-04 14:49:19 +0900

    d

--- 2014-fall/ProgrammingLanguage/hw4/hw4-2.ml
+++ 2014-fall/ProgrammingLanguage/hw4/hw4-2.ml
@@ -36,163 +36,190 @@ end;;
 
 let counter =
   let count = ref (-1) in
-    fun () -> incr count; string_of_int(!count)
+    fun () -> incr count; "!@#^" ^ string_of_int(!count)
 
 exception IMPOSSIBLE
 ;;
 
-let rec getEquations (env, map, operand) =
+(* operand -> string *)
+let rec operandToString operand =
+    match operand with
+    | I -> "I"
+    | Fn (operand1, operand2) -> operandToString(operand1) ^ " -> " ^ operandToString(operand2)
+    | V x -> "V " ^ x
+;;
+
+let rec mapToString (Map (operand1, operand2)) =
+    (operandToString operand1) ^ " |->" ^ (operandToString operand2)
+
+let rec equationToString (Equation (operand1, operand2)) =
+    (operandToString operand1) ^ " = " ^ operandToString(operand2)
+
+let rec equationListToString equationList =
+    "[ " ^ (List.fold_left (fun result equation -> result ^ (equationToString equation) ^" , ") "" equationList) ^ " ]\n";;
+
+let rec mapListToString mapList =
+    "[ " ^ (List.fold_left (fun result equation -> result ^ (mapToString equation) ^ " , ") "" mapList) ^ " ]\n";;
+
+let vars = ref []
+;;
+let newVars = 
+    function var -> vars := var::(!vars);
+;;
+let clearVars = function () -> vars := []
+;;
+
+(* return (env equation) *)
+let rec getEquationHelper (env, map, operand) =
     match map with
-    | (End StarBox) -> [Equation (operand, I)]
+    | (End StarBox) -> 
+        newVars(operand); 
+        newVars(I); 
+        [Equation (operand, I)]
     | (End (NameBox name)) ->
+            newVars(operand); 
+            newVars(V name); 
         (match (Env.lookup env name) with
-            | (Env.Null) -> [Equation (operand, (V name))]
+            | (Env.Null) -> 
+                [Equation (operand, (V name))]
             | (Env.Value operand1) -> [Equation (operand, operand1)])
     | (Branch (map1, map2)) ->
         let var1 = (V (counter())) in
-        (List.append (getEquations (env, map1, Fn(var1, operand))) (getEquations (env, map2, var1)))
+        (List.append (getEquationHelper (env, map1, Fn(var1, operand))) (getEquationHelper (env, map2, var1)))
     | (Guide (name, map1)) ->
         let var1 = (V (counter())) in
-        let var2 = (V (counter())) in
-	        Equation(operand, (Fn (var1, var2)))::getEquations((Env.bind env name (Env.Value var1)), map1, var2)
+        let var2 = (V (counter())) in 
+        let newEnv = (Env.bind env name (Env.Value var1)) in
+            newVars(var1); 
+	        Equation(operand, (Fn (var1, var2)))::getEquationHelper(newEnv, map1, var2)
 ;;
 
-(*
-(solution * operand) -> operand
-*)
+let rec getEquations map =
+    getEquationHelper(Env.empty, map, V "TAU")
+;;
+
+(*solution * operand) -> operand *)
+let rec substituteOperand (solutionList : solution list) (operand : operand) : operand=
+    let rec helper solution operand =
+    match operand with
+    | I -> I
+    | (Fn (operand1, operand2)) -> Fn ((helper solution operand1), (helper solution operand2))
+    | (V x) ->
+    (match solution with
+        | (Map (operand1, operand2)) -> if operand = operand1
+            then operand2
+            else operand) in
+    List.fold_left (fun operand solution -> helper solution operand) operand solutionList
+;;
+
+let rec substituteEquation solutionList (Equation (operand1, operand2)) : equation =
+    Equation((substituteOperand solutionList operand1), (substituteOperand solutionList operand2))
+;;
+
+let rec substituteEquations (solutionList:solution list) (equationList:equation list) : equation list=
+    List.map (substituteEquation solutionList) equationList
+;;
+
+let rec isElementOf (operand1 : operand) (operand2 : operand) : bool =
+  match operand2 with
+  | Fn (newOp1, newOp2) -> (isElementOf operand1 newOp1) || (isElementOf operand1 newOp2)
+  | x -> x = operand1
+;;
+
+
 let rec applySolution (solution, operand) =
     match operand with
     | I -> I
     | (Fn (operand1, operand2)) -> Fn ((applySolution (solution, operand1)), (applySolution (solution, operand2)))
     | (V x) ->
-    (match solution with
+        (match solution with
         | (Map (operand1, operand2)) -> if operand = operand1
-            then operand2
-            else operand)
+        then operand2
+        else operand)
 ;;
 
 let rec applySolutionList (solutionList, operand) =
     let helper operand solution = applySolution(solution, operand) in
     (List.fold_left
-        helper
-        operand
-        solutionList)
+     helper
+    operand
+    solutionList)
 ;;
 
 (* solutionList * solutionList -> solutionList *)
 let rec applySolutionListToSolutionList (solutionList1, solutionList2) =
-	let rec applySolutionToSolutionList (solution, solutionList) =
-	    solution::(List.map
-	        (function (Map (operand1, operand2)) ->
-	            Map (operand1, applySolution(solution, operand2)))
-	        solutionList) in
+    let rec applySolutionToSolutionList (solution, solutionList) =
+        solution::(List.map
+            (function (Map (operand1, operand2)) ->
+            Map (operand1, applySolution(solution, operand2)))
+        solutionList) in
 
     let helper result solution = (List.append (applySolutionToSolutionList(solution, solutionList2)) result) in
-    (List.fold_left
+        (List.fold_left
         helper
         []
         solutionList1)
 ;;
 
-let rec applySolutionListToEquationList (solutionList, equationList) =
-	let rec applySolutionToEqautionList (solution, equationList) =
-	    (List.map
-	        (function (Equation (operand1, operand2)) ->
-	            Equation (applySolution(solution, operand1), applySolution(solution, operand2)))
-	        equationList) in
 
-    let helper result solution = (List.append (applySolutionToEqautionList(solution, equationList)) result) in
-    (List.fold_left
-        helper
-        []
-        solutionList)
-;;
 
 (* operand * operand -> solutionList *)
-let rec unify (operand1, operand2) =
+let rec unify ((operand1:operand), (operand2:operand)) : solution list =
     if (operand1 = operand2)
         then []
         else
             (match (operand1, operand2) with
             | (Fn (t1, t2), Fn (t3, t4)) ->
                 let s1 = unify(t1, t3) in
-                let s2 = unify(applySolutionList(s1, t2), applySolutionList(s1, t4)) in
+                let s2 = unify((substituteOperand s1 t2), (substituteOperand s1 t4)) in
                 applySolutionListToSolutionList(s2, s1)
-           | (V a, t) -> [Map ((V a), t)]
-           | (t, V a) -> [(Map ((V a), t))]
-           | _ -> raise IMPOSSIBLE)
-(*
-let rec unify (operand1, operand2, solutionList) =
-    if (operand1 = operand2)
-        then solutionList
-        else
-            (match (operand1, operand2) with
-            | (Fn (t1, t2), Fn (t3, t4)) ->
-                let s1 = unify(t1, t3, solutionList) in
-                let s2 = unify(t2, t4, solutionList) in
-                let newSolutionList = applySolutionToSolutionList((List.hd s1), solutionList) in
-                applySolutionToSolutionList((List.hd s2), newSolutionList)
-           | (V a, t) -> print_string a;applySolutionToSolutionList((Map ((V a), t)), solutionList)
-           | (t, V a) -> print_string a;applySolutionToSolutionList((Map ((V a), t)), solutionList)
+           | (V a, t) -> 
+            if isElementOf (V a) t 
+                then raise IMPOSSIBLE
+                else [Map ((V a), t)]
+           | (t, V a) -> 
+            if isElementOf (V a) t 
+                then raise IMPOSSIBLE
+                else [Map ((V a), t)]
            | _ -> raise IMPOSSIBLE)
 ;;
-*)
-
 (* equationList * solutionList -> solutionList *)
 let rec unifyAll(equationList, solutionList) =
     match equationList with
+    | [] -> solutionList
+    | Equation(operand1, operand2)::[] -> applySolutionListToSolutionList((unify(operand1, operand2)), solutionList)
+    | equation::tl ->  
+        let newSolutionList = unifyAll([equation], solutionList) in
+        let newEqutionList = substituteEquations newSolutionList tl in
+            unifyAll(newEqutionList, newSolutionList)
+;;
+
+let rec remove_dups lst = match lst with
     | [] -> []
-    | (Equation (operand1, operand2))::[] -> unify(operand1, operand2)
-    | hd::tl ->  let newSolutionList = unifyAll([hd], solutionList) in
-        unifyAll(applySolutionListToEquationList(newSolutionList, equationList), newSolutionList)
+    | h::t -> h::(remove_dups (List.filter (fun x -> x<>h) t))
 ;;
 
+let rec eval solutions =
+	let rec makeKey operand =
+	    match operand with
+	    | I -> Bar
+	    | (V x) -> Bar
+	    | Fn (op1, op2) -> (Node ((makeKey op1), (makeKey op2))) in
 
-let m1 = (Branch (End (NameBox "x"), End(NameBox "x")))
-;;
-let m2 = (Guide  ("x", End(NameBox "x")))
-;;
-let m3 = (Branch ((Guide  ("x", End(NameBox "x"))), End StarBox))
-;;
-let m4 = (Branch (End (NameBox "x"),
-    (Branch (End (NameBox "y"),
-        (Branch (End (NameBox "z"),
-            End (StarBox)))))))
+    List.map (function (Map (op1, op2)) -> (makeKey op2)) solutions
 ;;
 
-let eq1 = getEquations (Env.empty, m1, (V "TAU"))
-;;
-let eq2 = getEquations (Env.empty, m2, (V "TAU"))
-;;
-let eq3 = getEquations (Env.empty, m3, (V "TAU"))
-;;
-let eq4 = getEquations (Env.empty, m4, (V "TAU"))
+let getSol map = 
+    let eq5 = getEquations(map) in
+    let sol5 = unifyAll(eq5, []) in sol5
 ;;
 
-
-let ans1 = unifyAll(eq1, [])
-let ans2 = unifyAll(eq2, [])
-let ans3 = unifyAll(eq3, [])
-let ans4 = unifyAll(eq4, [])
-(*
-if (Env.lookup Env.empty "x") = Env.Null
-    then print_int 10
-    else print_int 0
-        ;;
-if (Env.lookup (Env.bind Env.empty "x" (Env.Value (V "x"))) "x") = Env.Value (V "x")
-    then print_int 10
-    else print_int 0
-
-let _ = print_int( getTerms(0, (End StarBox), 0));;
-let _ = print_int( getTerms(0, (End (NameBox "x")), 0));;
-let _ = print_int( getTerms(0, (Branch ((End StarBox), (End StarBox))), 0));;
-let _ = print_int( getTerms(0, (Guide ("x", (End StarBox))), 0));;
-
-let ap1 = applySolutionToSolutionList ((Map ((V "X"), (V "Y"))), [Map ((V "A"), (V "X"))])
-let ap1 = applySolution (Map ((V "X"), (V "Y"))) (V "X")
+let getReady map =
+    clearVars();
+    let eq5 = getEquations(map) in
+    let sol5 = unifyAll(eq5, []) in 
+    let dups = eval(List.filter (fun (Map (op1, op2)) -> ((not (op1 = (V "TAU"))) && (List.mem op1 !vars))) sol5) in
+    remove_dups dups
 ;;
-let ap2 = applySolution (Map ((V "X"), (V "Y"))) I
-;;
-let ap3 = applySolution (Map ((V "X"), (V "Y"))) (Fn ((V "X"), (V "X")))
-;;
-*)
+
+Branch (Guide ("1", Guide ("3", Branch (End (NameBox "3"), End (StarBox)))), End (NameBox "2")) 

5222a068d285bf06f47c8233ad2b9bb2790788ff
Jun Gyoung Seong <jungyoung.seong@gmail.com>
2014-11-18 23:24:28 +0900

    d

--- 2014-fall/ProgrammingLanguage/hw6/Rozetta_skel/rozetta.ml
+++ 2014-fall/ProgrammingLanguage/hw6/Rozetta_skel/rozetta.ml
@@ -11,7 +11,7 @@ module Rozetta = struct
   let rec trans : Sm5.command -> Sonata.command =
           let counter =
             let count = ref (-1) in
-              fun () -> incr count; "!@#^" ^ string_of_int(!count) in
+              fun () -> incr count; "@" ^ string_of_int(!count) in
 
 		  let rec trans_value_obj value =
 		    (match value with
@@ -26,16 +26,22 @@ module Rozetta = struct
 		            in Sonata.Val sonata_value
 		        | Sm5.Id  str -> Sonata.Id str
 		        | Sm5.Fn (str, command) ->
-                    let kontinum_name = "@#kontinum" in
-                    let command1 = [Sonata.MALLOC; Sonata.BIND kontinum_name; Sonata.PUSH (Sonata.Id kontinum_name); Sonata.STORE] in
-                    let command2 = (trans command) in
-                    let command3 = [(Sonata.PUSH(Sonata.Id kontinum_name)); Sonata.LOAD; (Sonata.PUSH (Sonata.Val (Sonata.Z 999))); Sonata.MALLOC; Sonata.CALL] in
-                    let commands = List.append command1 (List.append command2 command3) in
-                        Sonata.Fn (str, commands)) in
+                    let randomName = counter() in
+                    let result = String.concat "_" [randomName; "result"] in
+                    let loc = String.concat "_" [randomName; "loc"] in
+                    let value = String.concat "_" [randomName; "value"] in
+                    let kontinum = String.concat "_" [randomName; "kontinum"] in
+
+                    let change_result_function_arg_order =
+                        [Sonata.BIND result; Sonata.BIND loc; Sonata.BIND value; Sonata.BIND kontinum]
+                        @ [Sonata.PUSH (Sonata.Id result); Sonata.PUSH(Sonata.Id kontinum); Sonata.PUSH (Sonata.Id value); Sonata.PUSH (Sonata.Id loc)] in
+                    let call = [Sonata.CALL] in
+                    let new_command = (trans command) @ change_result_function_arg_order @ call in
+                        Sonata.Fn(str, new_command)) in
 		    Sonata.PUSH sonata_obj in
 
-		  let rec trans_jtr_cmd command1 command2 tl =
-		    [Sonata.JTR ((List.append (trans command1) (trans tl)), (List.append (trans command2) (trans tl)))] in
+		  let rec trans_jtr_cmd command1 command2 =
+		    [Sonata.JTR ((trans command1), (trans command2))] in
 
 		  let rec trans_box_cmd int1 =
 		    Sonata.BOX int1 in
@@ -46,38 +52,30 @@ module Rozetta = struct
 		  let rec trans_bind_cmd str =
 		    Sonata.BIND str in
 
-          let rec trans_call_cmd rest_sonata_command =
-            let randomName=  counter() in
-            let temp_loc_name = String.concat " " [randomName; "_loc_name"] in
-            let temp_var_name = String.concat " " [randomName; "_var_name"] in
-            let temp_proc_name1 = String.concat " " [randomName; "_proc_name1"] in
-            let temp_return_name = String.concat " " [randomName; "rest_sonata_command"] in
+          let rec trans_call_cmd rest_sonata_command = 
+                let randomName = counter() in
+                let loc = String.concat "_" [randomName; "loc"] in
+                let value = String.concat "_" [randomName; "value"] in
+                let func1 = String.concat "_" [randomName; "function1"] in
+                let func2 = String.concat "_" [randomName; "function2"] in
+                let kontinum_formal_param = String.concat "_" [randomName; "kontinum_formal_param"] in
 
-            let command2_non_recursive_kontinum = [(Sonata.PUSH (Sonata.Fn (temp_return_name, rest_sonata_command)))] in (* if kontinum loc is -111, 0*)
-            let command2_recursive_kontinum = [(Sonata.PUSH (Sonata.Fn (temp_return_name, (List.append rest_sonata_command [(Sonata.PUSH(Sonata.Id "@#kontinum")); Sonata.LOAD; (Sonata.PUSH (Sonata.Val (Sonata.Z 1111))); Sonata.MALLOC; Sonata.CALL]))))] in (* if kontinum loc is not -111, 0 *)
+                let bind_functin_args = [Sonata.BIND loc; Sonata.BIND value; Sonata.BIND func1; Sonata.BIND func2] in
+                let push_kontinum_function_args = [Sonata.PUSH(Sonata.Fn (kontinum_formal_param, rest_sonata_command)); Sonata.PUSH (Sonata.Val (Sonata.Z 1111)); Sonata.MALLOC] in
+                let push_binded_function_args =  [Sonata.PUSH(Sonata.Id func2); Sonata.PUSH(Sonata.Id func1); Sonata.PUSH (Sonata.Id value); Sonata.PUSH (Sonata.Id loc)] in
+                let call = [Sonata.CALL] in
 
-            let command1 = [Sonata.BIND temp_loc_name; Sonata.BIND temp_var_name; Sonata.BIND temp_proc_name1] in
-            let command2 = [Sonata.PUSH (Sonata.Val (Sonata.L (-111, 0))); Sonata.PUSH (Sonata.Id "@#kontinum"); Sonata.EQ; Sonata.JTR (command2_non_recursive_kontinum, command2_recursive_kontinum)] in
-            let command3 = [Sonata.PUSH(Sonata.Id temp_proc_name1); Sonata.PUSH (Sonata.Id temp_var_name); Sonata.PUSH (Sonata.Id temp_loc_name); Sonata.CALL] in
-            let command4 = [] in
-            List.append command1 (List.append command2 (List.append command3 command4)) in
-(*
-            let rest_command
-            PUSH 나올 수 없는 임의의 바리어블ㅡ 앞으로 나올 trans(sm5커맨드)한 것을
-            이놈을 메모리에 저장
-            그 로케이션을 미리 정의한 메모리 영역에 기록.
-            매모리 값 읽어서 환경에 컨티넘을 써줌.
-*)
+                bind_functin_args @ push_kontinum_function_args @ push_binded_function_args @ call
+          in
 
-    let emptyContinum = ref(0) in
-    fun command -> 
+    fun command ->
         match command with
         | [] -> []
 	    | (Sm5.PUSH obj)::tl -> trans_push_cmd obj::trans(tl)
 	    | Sm5.POP::tl -> Sonata.POP::trans(tl);
 	    | Sm5.STORE::tl -> Sonata.STORE::trans(tl);
 	    | Sm5.LOAD::tl -> Sonata.LOAD::trans(tl);
-	    | (Sm5.JTR (command1, command2))::tl -> (trans_jtr_cmd command1 command2 tl);
+	    | (Sm5.JTR (command1, command2))::tl ->  [Sonata.JTR ((trans (command1@tl)), (trans (command2@tl)))];
 	    | Sm5.MALLOC::tl -> Sonata.MALLOC::trans(tl);
 	    | (Sm5.BOX int1)::tl -> (trans_box_cmd int1)::trans(tl);
 	    | (Sm5.UNBOX str)::tl -> (trans_unbox_cmd str)::trans(tl);
@@ -86,11 +84,6 @@ module Rozetta = struct
 	    | (Sm5.GET)::tl -> (Sonata.GET)::trans(tl);
 	    | (Sm5.PUT)::tl -> (Sonata.PUT)::trans(tl);
 	    | (Sm5.CALL)::tl -> 
-            if !emptyContinum = 0
-                then
-                    (print_endline "@@@@@@@@@@@@@@@@@@@@@@@@@@";
-                    incr emptyContinum; List.append [Sonata.PUSH (Sonata.Val (Sonata.L (-111, 0))); Sonata.BIND "@#kontinum"] (trans_call_cmd (trans tl)))
-                else
                     (trans_call_cmd (trans tl)) 
 	    | (Sm5.ADD)::tl -> (Sonata.ADD)::trans(tl);
 	    | (Sm5.SUB)::tl -> (Sonata.SUB)::trans(tl);
